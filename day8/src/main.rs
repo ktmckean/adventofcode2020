d5fba3ca86320e05b8af3c647298b658a677e4ed
Kerry McKean <kmckean@alteryx.com>
2020-12-08 10:27:50 -0700

    factor out part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -25,89 +25,98 @@ fn readLines(path :&str) -> std::vec::Vec<String>{
 
 fn main() {
     let v = readLines("C:\\Repos\\adventofcode\\2020\\day8\\src\\input.txt");
+    let mut code = parseInputToCode(&v);
 
-    let mut linesToChange = Vec::<usize>::new();
-    for (i,line) in v.iter().enumerate(){
-        let insVal :Vec<&str> = v[i as usize].split(" ").collect();
-        if insVal[0] == "nop" || insVal[0] == "jmp"{
-            linesToChange.push(i);
-        }
-    }
-
-
-    for changed in linesToChange
-    {
-        let mut acc :i64 = 0;
-        let mut i : i64 = 0;
-        let mut visited : HashSet<i64> = HashSet::new();
-        while i >= 0 && (i as usize) < v.len()  {
-            if visited.contains(&i){
-                println!("{}", acc);
-                break;
-            }
-            visited.insert(i);
-
-            let insVal :Vec<&str> = v[i as usize].split(" ").collect();
-            let val = insVal[1].parse::<i64>().unwrap();
-
-            let mut ins : &str = insVal[0];
-            if changed == (i as usize){
-                if insVal[0] == "nop"{
-                    ins = "jmp";
-                } else if insVal[0] == "jmp"{
-                    ins = "nop";
-                }
-            }
-
-            if ins == "acc"{
-                acc += val;
-                i += 1;
-            }
-            if ins == "jmp"{
-                i += val;
-            }
-            if ins == "nop"{
-                i += 1;
-                continue;
-            }
-        }
-        if i < 0{
-            println!("i less than 0! {}", i);
-            continue;
-        }
-        if i as usize == v.len(){
-            println!("Accumulator: {}", acc);
-            break;
-        }
-    }
-
+    doPart1(&code);
+    doPart2(&mut code);
 }
 
-// fn executeProgram(code :&Vec<(&str,i64)>){
-//     let mut acc :i64 = 0;
-//     let mut i : i64 = 0;
-//     let mut visited : HashSet<i64> = HashSet::new();
-//     while i >= 0 && (i as usize) < code.len()  {
-//         if visited.contains(&i){
-//             println!("{}", acc);
-//             break;
-//         }
-//         visited.insert(i);
+fn doPart2(code: &mut Vec::<(&str,i64)>){
+    let mut linesToChange = Vec::<usize>::new();
+    for i in 0..code.len() {
+        if swapCmdIfJmpNop(i, &mut code[i]){
+            linesToChange.push(i);
 
-//         let insVal :Vec<&str> = v[i as usize].split(" ").collect();
-//         let val = insVal[1].parse::<i64>().unwrap();
-//         let ins : &str = insVal[0];
+            let (finali,acc) = executeProgram(&code);
+            if finali < 0{
+                println!("Error!  Negative i: {}",i);
+                return;
+            }
+            else if finali as usize == code.len(){
+                println!("Part 2 answer: {}",acc);
+            }
+
+            // Swap the commands back
+            swapCmdIfJmpNop(i, &mut code[i]);
+        }
+    }
+    println!("No swappable instruction found that provides part2 solution.");
+}
+
+fn swapCmdIfJmpNop(i: usize, cmd: &mut (&str,i64)) -> bool {
+    match cmd.0{
+        "nop" => {
+            cmd.0 = "jmp";
+            return true;
+        },
+        "jmp" => {
+            cmd.0 = "nop";
+            return true;
+        }
+        _ => {},
+    }
+    return false;
+
+    // if code[i].0 == "nop"{
+    //     code[i].0 = "jmp";
+    // } 
+    // else if code[i].0 == "jmp"{
+    //     code[i].0 = "nop";
+    // }
+}
+
+fn doPart1(code: &Vec::<(&str,i64)>){
+    let (i,acc) = executeProgram(&code);
+    println!("Part 1 answer: {}",acc);
+}
+
+fn executeProgram(code :&Vec<(&str,i64)>) -> (i64,i64){
+    let mut acc :i64 = 0;
+    let mut i : i64 = 0;
+    let mut visited : HashSet<i64> = HashSet::new();
+    while i >= 0 && (i as usize) < code.len()  {
+        if visited.contains(&i){
+            // println!("Final acc: {}", acc);
+            break;
+        }
+        visited.insert(i);
+
+        let ins = code[i as usize].0;
+        let val = code[i as usize].1;
         
-//         if ins == "acc"{
-//             acc += val;
-//             i += 1;
-//         }
-//         if ins == "jmp"{
-//             i += val;
-//         }
-//         if ins == "nop"{
-//             i += 1;
-//             continue;
-//         }
-//     }
-// }
+        if ins == "acc"{
+            acc += val;
+            i += 1;
+        }
+        if ins == "jmp"{
+            i += val;
+        }
+        if ins == "nop"{
+            i += 1;
+            continue;
+        }
+    }
+    return (i, acc);
+}
+
+fn parseInputToCode(v: &Vec<String>) -> Vec::<(&str,i64)>{
+    let mut code = Vec::<(&str,i64)>::new();
+    
+    for line in v.iter(){
+        let insVal :Vec<&str> = line.split(" ").collect();
+        let val = insVal[1].parse::<i64>().unwrap();
+        let ins : &str = insVal[0];
+        code.push((ins,val));
+    }
+    code
+}
