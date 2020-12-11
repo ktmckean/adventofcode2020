c97d02b2214ae4d5023e7baabd8003de2e737549
ktmckean <mckeankt@gmail.com>
2020-12-10 22:03:23 -0700

    resolve day7 conflict

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -26,58 +26,34 @@ fn readLines(path :&str) -> std::vec::Vec<String>{
     v
 }
 
+struct Bag{
+    color : String,
+    children : Vec<(Bag,i32)>
+}
+
+impl Bag{
+    fn fromTupleVec(color: &String, children: Vec<(Bag,i32)>) -> Bag{
+        return Bag {color:color, children:children};   
+    }
+
+    fn fromStringVec(color: &String, children: &Vec<&str>) -> Bag{
+        let mut tupleVec = Vec<(Bag,i32)>::new();
+        for child in children{
+            let num :i32 = bagNums[..1].parse::<i32>().unwrap();
+            let color :&str= &bagNums[2..];
+            tupleVec.push(color.to_string(), Vec<(Bag,i32)>::new());
+        }
+        return Bag {color:color, children:tupleVec};
+    }
+
+}
+
 fn main() {
     doPart1();
 
     let v = readLines("C:\\Users\\Kerry\\coding\\aoc2020\\day7\\src\\input.txt");
-
-    let mut bags : HashMap::<String, HashMap<String, i32>> = HashMap::new();
-
-    for line in &v{
-        let mut colors = line.replace(" bags contain ","\n");
-        colors = colors.replace(" bag, ",",");
-        colors = colors.replace(" bags, ",",");
-        colors = colors.replace(" bag.","");
-        colors = colors.replace(" bags.","");
-        colors = colors.replace("no other","");
-        // println!("{}",colors);
-
-        let bagsPair :Vec<&str>= colors.split("\n").collect();
-        let bigBag:&str = &bagsPair[0];
-        let littleBags:Vec<&str> = bagsPair[1].split(",").collect();
-
-        if !bags.contains_key(bigBag){
-            bags.insert(bigBag.to_string(), HashMap::new());
-        }
-        if littleBags[0].is_empty(){
-            // println!("No children! {}",bigBag);
-            continue;
-        }
-
-        for bagNums in &littleBags {
-            // println!("{}",&bagNums[..1]);
-            let num :i32 = bagNums[..1].parse::<i32>().unwrap();
-            let bag :&str= &bagNums[2..];
-
-            if bags[bigBag].contains_key(bag){
-                // we're good
-                continue;
-            } else {
-                bags.get_mut(bigBag).unwrap().insert(bag.to_string(), num);
-            }
-        }
-
-    }
-
-    let myBagType = "shiny gold";
-    let bagsCount : i64 = addAllChildBags(myBagType, &bags) - 1;    // -1 because we don't count our top bag.
-    // let mut topBags = HashSet::<String>::new();
-    // for bag in &bags[myBagType]{
-    //     println!("{}",bag.0);
-    //     addAllChildBags(&bag, &bags, &mut bagsCount);
-    // }
-
-    println!("Part 2: {}", bagsCount);
+    // doPart2();
+ 
 
 }
 
@@ -102,7 +78,7 @@ fn doPart1(){
         let littleBags:Vec<&str> = bagsPair[1].split(",").collect();
 
         if littleBags[0].is_empty(){
-            // println!("Skipped! {}",bigBag);
+            println!("Skipped! {}",bigBag);
             continue;
         }
 
@@ -126,8 +102,79 @@ fn doPart1(){
         addAllParentBagsPart1(&bag, &bags, &mut topBags);
     }
 
-    println!("Part1: {}", topBags.len());
+    println!("Part 2: {}", bagsCount);
+}
 
+fn doPart2Old(){
+    let mut bags : HashMap::<String, HashMap<String, i32>> = HashMap::new();
+    let v = readLines("C:\\Users\\Kerry\\coding\\aoc2020\\day7\\src\\input.txt");
+
+    for line in &v{
+        let mut colors = line.replace(" bags contain ","\n");
+        colors = colors.replace(" bag, ",",");
+        colors = colors.replace(" bags, ",",");
+        colors = colors.replace(" bag.","");
+        colors = colors.replace(" bags.","");
+        colors = colors.replace("no other","");
+        // println!("{}",colors);
+
+        let bagsPair :Vec<&str>= colors.split("\n").collect();
+
+
+        let bigBag:&str = &bagsPair[0];
+        let littleBags:Vec<&str> = bagsPair[1].split(",").collect();
+
+        if littleBags[0].is_empty(){
+<<<<<<< Updated upstream
+            // println!("Skipped! {}",bigBag);
+=======
+            // println!("No children! {}",bigBag);
+>>>>>>> Stashed changes
+            continue;
+        }
+        for bagNum in littleBags{
+            let num :i32 = bagNums[..1].parse::<i32>().unwrap();
+            let bag :&str= &bagNums[2..];
+        }
+
+        if !bags.contains_key(bigBag){
+            bags.insert(bigBag.to_string(), HashMap::new());
+        }
+
+
+        for bagNums in &littleBags {
+            // println!("{}",&bagNums[..1]);
+            let num :i32 = bagNums[..1].parse::<i32>().unwrap();
+            let bag :&str= &bagNums[2..];
+
+            if bags[bigBag].contains_key(bag){
+                // we're good
+                continue;
+            } else {
+                bags.get_mut(bigBag).unwrap().insert(bag.to_string(), num);
+            }
+        }
+
+    }
+
+    let myBagType = "shiny gold";
+<<<<<<< Updated upstream
+    let mut topBags = HashSet::<String>::new();
+    for bag in &bags[myBagType]{
+        addAllParentBagsPart1(&bag, &bags, &mut topBags);
+    }
+
+    println!("Part1: {}", topBags.len());
+=======
+    let bagsCount : i64 = addAllChildBags(myBagType, &bags) - 1;    // -1 because we don't count our top bag.
+    // let mut topBags = HashSet::<String>::new();
+    // for bag in &bags[myBagType]{
+    //     println!("{}",bag.0);
+    //     addAllChildBags(&bag, &bags, &mut bagsCount);
+    // }
+>>>>>>> Stashed changes
+
+    println!("{}", bagsCount);
 }
 
 fn addAllChildBags(bag: &str, rules :&HashMap::<String,HashMap<String,i32>>) -> i64{
