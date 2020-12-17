9cf274f2bcb4925e09be3576a64bd92f108d395e
ktmckean <mckeankt@gmail.com>
2020-12-16 23:45:57 -0700

    day17 pt 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -24,6 +24,11 @@ fn readLines(path :&str) -> std::vec::Vec<String>{
 }
 
 
+// struct ptnd{
+//     pub ndim : usize;
+//     pub coords : Vec<i32>;
+// }
+
 
 fn main() {
     // doPartOne();
@@ -31,29 +36,30 @@ fn main() {
     let v = readLines("C:\\Users\\Kerry\\coding\\aoc2020\\day17\\src\\input.txt");
 
     let numTurns = 6;
-
+    // let nDim = 4;
     let gridSize = numTurns*2 + v.len();
 
     // let mut grid = Vec::<Vec<Vec<bool>>>::new();
-    let mut grid = vec![vec![vec![0 ; gridSize]; gridSize]; gridSize];
+    let mut grid: Vec<Vec<Vec<Vec<i32>>>> = vec![vec![vec![vec![0 ; gridSize]; gridSize]; gridSize]; gridSize];
     // for yzplane in &grid{
     //     for mut zcol in yzplane{
     //         zcol = vec![false; gridSize];
     //     }
     // }
 
-    println!("{},{},{}",grid.len(), grid[0].len(), grid[0][0].len());
+    // println!("{},{},{}",grid.len(), grid[0].len(), grid[0][0].len());
 
     for (i,line) in v.iter().enumerate(){
         for (j,c) in line.chars().enumerate(){
             if c == '#'{
-                grid[gridSize/2][i+numTurns][j+numTurns] = 1;
+                grid[gridSize/2][gridSize/2][i+numTurns][j+numTurns] = 1;
             }
         }
     }
 
     // printGrid(&grid);
     for _i in 0..numTurns{
+        println("update {} ...", i);
         update(&mut grid);
     }
 
@@ -62,34 +68,81 @@ fn main() {
     for x in 0..grid.len(){
         for y in 0..grid.len(){
             for z in 0..grid.len() {
-                if grid[x][y][z] != 0{
-                    numActive += 1;
+                for w in 0..grid.len(){
+                    if grid[x][y][z][w] != 0{
+                        numActive += 1;
+                    }
                 }
             }
         }
     }
-    println!("part1: {}", numActive);
+    println!("part 2: {}", numActive);
 
 }
 
+// fn doPartOne(){
+//     let v = readLines("C:\\Users\\Kerry\\coding\\aoc2020\\day17\\src\\input.txt");
 
-fn update(grid: &mut  Vec::<Vec<Vec<i32>>>){
+//     let numTurns = 6;
+
+//     let gridSize = numTurns*2 + v.len();
+
+//     // let mut grid = Vec::<Vec<Vec<bool>>>::new();
+//     let mut grid = vec![vec![vec![0 ; gridSize]; gridSize]; gridSize];
+//     // for yzplane in &grid{
+//     //     for mut zcol in yzplane{
+//     //         zcol = vec![false; gridSize];
+//     //     }
+//     // }
+
+//     println!("{},{},{}",grid.len(), grid[0].len(), grid[0][0].len());
+
+//     for (i,line) in v.iter().enumerate(){
+//         for (j,c) in line.chars().enumerate(){
+//             if c == '#'{
+//                 grid[gridSize/2][i+numTurns][j+numTurns] = 1;
+//             }
+//         }
+//     }
+
+//     // printGrid(&grid);
+//     for _i in 0..numTurns{
+//         update(&mut grid);
+//     }
+
+
+//     let mut numActive = 0;
+//     for x in 0..grid.len(){
+//         for y in 0..grid.len(){
+//             for z in 0..grid.len() {
+//                 if grid[x][y][z] != 0{
+//                     numActive += 1;
+//                 }
+//             }
+//         }
+//     }
+//     println!("part1: {}", numActive);
+// }
+
+fn update(grid: &mut  Vec<Vec<Vec<Vec<i32>>>>){
     let gridSize = grid.len();
     let mut updated = grid.clone();//vec![vec![vec![0 ; gridSize]; gridSize]; gridSize];
     
     for x in 0..grid.len(){
         for y in 0..grid.len(){
             for z in 0..grid.len() {
-                let numActiveAdj = getNumAdjacent(grid, x as i64,y as i64,z as i64);
-                // println!("numadj {}", numActiveAdj);
+                for w in 0..grid.len(){
+                    let numActiveAdj = getNumAdjacent(grid, x as i64,y as i64,z as i64, w as i64);
+                    // println!("numadj {}", numActiveAdj);
 
-                if grid[x][y][z] != 0{
-                    if !(numActiveAdj == 2 || numActiveAdj == 3){
-                        updated[x][y][z] = 0;
+                    if grid[x][y][z][w] != 0{
+                        if !(numActiveAdj == 2 || numActiveAdj == 3){
+                            updated[x][y][z][w] = 0;
+                        }
                     }
-                }
-                else if numActiveAdj == 3{
-                    updated[x][y][z] = 1;
+                    else if numActiveAdj == 3{
+                        updated[x][y][z][w] = 1;
+                    }
                 }
             }
         }
@@ -102,31 +155,33 @@ fn update(grid: &mut  Vec::<Vec<Vec<i32>>>){
 
 }
 
-fn printGrid(grid: &Vec::<Vec<Vec<i32>>>){
-    // assert!(grid != updated);
+// fn printGrid(grid: &Vec::<Vec<Vec<Vec<i32>>>>){
+//     // assert!(grid != updated);
     
-    for yz in grid{
-        // print!("[");
-        for y in yz{
-            for z in y{
-                print!("{:?}", z);
-            }
-            print!("]\n");
-        }
-        print!("]\n");
+//     for yz in grid{
+//         // print!("[");
+//         for y in yz{
+//             for z in y{
+//                 print!("{:?}", z);
+//             }
+//             print!("]\n");
+//         }
+//         print!("]\n");
 
-    }
-    println!("break");
-}
+//     }
+//     println!("break");
+// }
 
 
-fn getNumAdjacent(grid: &Vec<Vec<Vec<i32>>>, x: i64, y: i64, z: i64) -> i32{
+fn getNumAdjacent(grid: &Vec<Vec<Vec<Vec<i32>>>>, x: i64, y: i64, z: i64, w: i64) -> i32{
     let mut lowx :i64 = -1;
     let mut lowy :i64= -1;
     let mut highx:i64 = 1;
     let mut highy:i64 = 1;
     let mut lowz :i64= -1;
     let mut highz:i64 = 1;
+    let mut loww :i64= -1;
+    let mut highw:i64 = 1;
 
     if x==0{
         lowx = 0;
@@ -145,25 +200,35 @@ fn getNumAdjacent(grid: &Vec<Vec<Vec<i32>>>, x: i64, y: i64, z: i64) -> i32{
     } else if z as usize== grid[0][0].len() - 1{
         highz = 0;
     }
+    if w==0{
+        loww = 0;
+    } else if w as usize== grid[0][0][0].len() - 1{
+        highw = 0;
+    }
+
 
     let mut numActive:i32 = 0;
     for i in lowx .. highx + 1{
         for j in lowy .. highy + 1{
             for k in lowz .. highz + 1{
-                if i==0 && j ==0 && k == 0{
-                    continue;
-                }
+                for l in loww .. highw + 1{
+                    if i==0 && j ==0 && k == 0 && l==0{
+                        continue;
+                    }
 
-                assert! (x+i >= 0);
-                assert! (x+i < grid.len() as i64);
-                assert! (y+j >= 0);
-                assert! (y+j < grid[0].len() as i64);
-                assert! (z+k >= 0);
-                assert! (z+k < grid[0][0].len() as i64);
+                    assert! (x+i >= 0);
+                    assert! (x+i < grid.len() as i64);
+                    assert! (y+j >= 0);
+                    assert! (y+j < grid[0].len() as i64);
+                    assert! (z+k >= 0);
+                    assert! (z+k < grid[0][0].len() as i64);
+                    assert! (w+l >= 0);
+                    assert! (w+l < grid[0][0].len() as i64);
 
-                if grid[(x+i) as usize][(y+j) as usize][(z+k) as usize] != 0
-                {
-                    numActive+=1;
+                    if grid[(x+i) as usize][(y+j) as usize][(z+k) as usize][(w+l) as usize] != 0
+                    {
+                        numActive+=1;
+                    }
                 }
             }
         }
