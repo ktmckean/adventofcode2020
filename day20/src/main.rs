fd02de84c0d087350b3077471216012c1de2c1b3
ktmckean <mckeankt@gmail.com>
2020-12-20 00:03:52 -0700

    day20 add and test rotation

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -32,7 +32,7 @@ fn readLines(path :&str) -> std::vec::Vec<String>{
 fn main() {
     // let v = readLines("C:\\Users\\Kerry\\coding\\aoc2020\\day18\\src\\input.txt");
     doPartOne();
-    // doPartTwo();
+    doPartTwo();
 }
 
 static TileSize : usize = 10;
@@ -45,28 +45,7 @@ fn doPartOne(){
 
     let tiles = readTiles(&v);
 
-    let mut edgeNums = HashMap::<Edge, usize>::new();
-
-
-    for t in &tiles{
-        for mut e in getEdges(&t.1){
-            if !edgeNums.contains_key(&e){
-                edgeNums.insert(e.clone(), 1);
-            }
-            else {
-                *edgeNums.get_mut(&e).unwrap() += 1;
-            }
-
-            e.reverse();
-            if !edgeNums.contains_key(&e){
-                edgeNums.insert(e.clone(), 1);
-            }
-            else {
-                *edgeNums.get_mut(&e).unwrap() += 1;
-            }
-
-        }
-    }
+    let mut edgeNums = getEdgeNums(&tiles);
 
     let mut corners = Vec::<usize>::new();
 
@@ -93,22 +72,58 @@ fn doPartOne(){
         prod *= id as i64;
     }
     println!("Part 1: {}", prod);
-
-    let pic = Vec::<Vec<Tile>>::new();
-
-    // println!("{}", tiles.len());
-    // for e in edgeNums{
-    //     println!("{}", e.1);
-    //     if e.1 > 2{
-    //         println!("Alert!  Found a high number edge!");
-    //     }
-    // }
-
 }
 
-// fn createImage(tiles: &HashMap<usize, Tile>, ) -> Tile {
+fn doPartTwo(){
+    let v = readLines("C:\\Users\\Kerry\\coding\\aoc2020\\day20\\src\\input.txt");
 
-// }
+    testRotate();
+
+    let tiles = readTiles(&v);
+    let mut edgeNums = getEdgeNums(&tiles);
+
+
+    // println!("Part 1: {}", prod);
+}
+
+fn createImage(tiles: &HashMap<usize, Tile>, ) -> Tile {
+    let edgeNums = getEdgeNums(&tiles);
+
+
+    return Tile::new();
+}
+
+fn placeTiles(tiles: &HashMap<usize, Tile>) -> Vec<Vec<Tile>>{
+
+
+    let mut retTile: Vec<Vec<Tile>> = Vec::new();//= vec![ vec![ '.'; tile.len()]; tile.len()];//Vec::with_capacity(tile.len());
+    return retTile;
+}
+
+fn testRotate(){
+    let mut tile = Tile::new();
+    tile.push(['#','.','.'].to_vec());
+    tile.push(['.','.','.'].to_vec());
+    tile.push(['.','.','.'].to_vec());
+    assert!(rotate(&rotate(&rotate(&rotate(&tile)))) == tile);
+
+    let mut tile2 = tile.clone();
+    tile2.remove(0);
+    tile2.push(['.','.','#'].to_vec());
+    assert!(&rotate(&rotate(&tile)) == &tile2);
+}
+
+fn rotate(tile: &Tile) -> Tile {
+    let mut retTile: Tile = vec![ vec![ '.'; tile.len()]; tile.len()];//Vec::with_capacity(tile.len());
+    // retTile.resize()
+
+    for (j,row) in tile.iter().enumerate() {
+        for (i,cell) in row.iter().rev().enumerate() {
+            retTile[tile.len()-1 - i][j] = tile[j][i];
+        }
+    }
+    return retTile;
+}
 
 fn getEdgeNums(tiles: &HashMap<usize, Tile>) -> HashMap<Edge, usize>{
     let mut edgeNums = HashMap::<Edge, usize>::new();
